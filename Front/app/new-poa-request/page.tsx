cbdb8343b6681bf2885bd306b82f0ef32acedb4f
Nebil <nebilbromance@gmail.com>
2025-11-21 23:47:47 +0300

    Update page.tsx

--- Front/app/new-poa-request/page.tsx
+++ Front/app/new-poa-request/page.tsx
@@ -70,8 +70,8 @@ export default function NewPOARequestPage() {
         <Sidebar />
       </div>
 
-      {/* Main content: added ml-64 to prevent sidebar overlap */}
-      <div className="flex-1 flex flex-col min-w-0 ml-64">
+      {/* Main content: flex-1 automatically fills remaining space */}
+      <div className="flex-1 flex flex-col min-w-0">
         <Header />
 
         <main className="flex-1 overflow-auto p-6">
